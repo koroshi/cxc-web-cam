5c61a732334f8c42a2689c25577ec274bcab7ca3
koroshi <stlcxc@163.com>
2025-07-16 15:15:15 +0800

    change config for gitpage

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: '../docs'
-  /* config options here */
+  // 告诉 Next.js 应用将部署在 GitHub Pages 的子路径下
+  basePath: "/cxc-web-cam",
+
+  // 静态资源路径前缀
+  assetPrefix: "/cxc-web-cam/",
+
+  // 导出为静态 HTML 文件
+  output: "export",
+
+  // 指定输出目录为父级目录的 docs 文件夹
+  distDir: "../docs",
+
+  // 其他配置选项...
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
